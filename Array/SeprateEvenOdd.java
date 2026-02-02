01ea680e57480d8b2c9882ff29a88fdf25340b8d
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-02-02 23:32:50 +0530

    a

---
+++ Array/SeprateEvenOdd.java
@@ -0,0 +1,37 @@
+package Array;
+
+public class SeprateEvenOdd {
+    public static void seprateEvenOdd(int arr[]){
+       int n=arr.length;
+        int x=0, y=0;
+        int odd[]=new int[x];
+        int  even[]=new int[y];
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                even[x]=arr[i];
+                x++;
+            }else{
+                odd[y]=arr[i];
+                y++;
+            }
+
+        }
+        for(int i:even){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for(int j:odd){
+            System.out.print(j+" ");
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        seprateEvenOdd(arr);
+    }
+    
+}
