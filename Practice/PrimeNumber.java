e3ce07f8ce70b002fafecf450559a368cde5a361
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-02-09 22:46:05 +0530

    prime Number

---
+++ Practice/PrimeNumber.java
@@ -0,0 +1,26 @@
+package Practice;
+
+public class PrimeNumber {
+public static void primeNumber(int n){
+for(int i=1;i<=n;i++){
+    int count=0;
+    for(int j=1;j<=n;j++){
+        if(i%j==0){
+            count++;
+        }
+
+    }
+    if(count==2){
+        System.out.print(i+" ");
+    }
+
+}
+}
+
+
+
+    public static void main(String[] args) {
+        primeNumber(100);
+    }
+    
+}
