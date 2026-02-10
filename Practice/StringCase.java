939e14a3fcd05800a316265260f9e63fac46c059
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-02-10 12:06:28 +0530

    a

---
+++ Practice/StringCase.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class StringCase {
+    public static void main(String[] args) {
+        String s1="java";
+        String s2=s1;
+        s1="Python";
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+    
+}
