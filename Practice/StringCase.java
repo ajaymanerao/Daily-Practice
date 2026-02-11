9b4b6ff3b16b9ddf4ded90a02e71f674eaeac403
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-02-11 23:33:50 +0530

    A

--- Practice/StringCase.java
+++ Practice/StringCase.java
@@ -11,9 +11,10 @@ public class StringCase {
         System.out.println(s2);
         String s4=new String("Ajay");
         String s5=new String("Ajay");
-       System.out.println("Does s1 equal s3? " + s1.equals(s3)); // false ("Python" vs "java")}
-
-       System.out.println("Does s4 equal s5? " + s4.equals(s5)); // true (Content is same)
+        System.out.println("Does s2 equal s3? "+s2==s3);
+        System.out.println("Does s2 equal s3? "+s2.equals(s3));
+        System.out.println("Does s1 equal s3? " + s1.equals(s3)); // false ("Python" vs "java")}
+        System.out.println("Does s4 equal s5? " + s4.equals(s5)); // true (Content is same)
         System.out.println("Is s4 the same object as s5? " + (s4 == s5)); // false (Different heap objects)
          
     }
