8b39e6127fdd76d6a0f743008aadcb4f1e233b88
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-02-21 00:11:05 +0530

    a

--- Practice/RemoveDuplicate.java
+++ Practice/RemoveDuplicate.java
@@ -1,24 +1,24 @@
 package Practice;
-
-import java.util.Arrays;
-
-public class UniqueElement {
-    public static void UniqueElement(int arr[]){
+import java.util.*;
+public class RemoveDuplicate {
+    public static void removeDuplicateElement(int arr[]){
         Arrays.sort(arr);
         for(int i=0;i<arr.length;i++){
             int count=0;
-            for(int j=0;j<arr.length;j++){
+            for(int j=i;j<arr.length;j++){
                 if(arr[i]==arr[j]){
                     count++;
                 }
             }
-            if(count==1){
+            i=i+(count-1);
             System.out.print(arr[i]+" ");
-            }
         }
     }
     public static void main(String[] args) {
         int arr[]={1,2,3,1,2,4,5,4,6,7,8,2};
-        UniqueElement(arr);
+        removeDuplicateElement(arr);
     }
 }
+
+    
+ 
