ebd395cc4dd72f7dce04ef20eafdf7238dd732bc
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-06 14:41:37 +0530

    a

---
+++ multithrading/Example1.java
@@ -0,0 +1,50 @@
+package multithrading;
+
+class Counter{
+    int cnt=0;
+    public void increment(){
+        cnt++;
+    }
+}
+
+
+class Thread1 extends Thread{
+    Counter counter;
+    public Thread1(Counter counter){
+        this.counter=counter;
+    }
+
+    public void run(){
+        for(int i=0;i<=100;i++){
+            counter.increment();
+        }
+    }
+}
+
+class Thread2 extends Thread{
+    Counter counter;
+    public Thread2(Counter counter){
+        this.counter=counter;
+    }
+
+    public void run(){
+        for(int i=0;i<=100;i++){
+            counter.increment();
+        }
+    }
+}
+
+public class Example1 {
+    public static void main(String[] args) {
+        
+    
+    Counter c=new Counter();
+    Thread1 t1=new Thread1(c);
+    Thread2 t2=new Thread2(c);
+    t1.start();
+    t2.start();
+    //Thread.sleep(300);
+    System.out.println(c.cnt);
+    }
+    
+}
