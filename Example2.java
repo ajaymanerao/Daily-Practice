3862635dc48fe5bfdbe70d497c1c1a3d472ddb9b
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-09 23:46:54 +0530

    a

--- Example2.java
+++ Example2.java
@@ -32,30 +32,30 @@ class Thread2 extends Thread{
     }
 }
 
-// class Thread3 extends Thread{
-//     Counter counter;
-//     public Thread3(Counter counter){
-//         this.counter=counter;
-//     }
+class Thread3 extends Thread{
+    Counter counter;
+    public Thread3(Counter counter){
+        this.counter=counter;
+    }
 
-//     public void run(){
-//         for(int i=0;i<=100;i++){
-//             counter.increment();
-//         }
-//     }
-// }
+    public void run(){
+        for(int i=0;i<=100;i++){
+            counter.increment();
+        }
+    }
+}
 
-public class Example1 {
+public class Example2 {
     public static void main(String[] args) {
         
     
     Counter c=new Counter();
     Thread1 t1=new Thread1(c);
     Thread2 t2=new Thread2(c);
-    //Thread3 t3=new Thread3(c);
+    Thread3 t3=new Thread3(c);
     t1.start();
     t2.start();
-   // t3.start();
+    t3.start();
     //Thread.sleep(300);
     System.out.println(c.cnt);
     }
