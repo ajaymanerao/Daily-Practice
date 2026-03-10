3bf0bca12e973521b22c8d302d7b7e0841832fb0
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-10 14:51:00 +0530

    collection

--- collections/ArraysList.java
+++ collections/ArraysList.java
@@ -18,6 +18,43 @@ public class ArraysList {
         System.out.println("E remove(index) "+family.remove(0));
         System.out.println(family);
 
+
+        ArrayList<String>family1=new ArrayList<String>();
+        family1.add("Raju");
+        family1.add("Kareeena");
+        family1.add("Shaeukh");
+        family.add("Ravi");
+
+        family.addAll(family1);
+        System.out.println(family);
+
+
+        System.out.println("Contains All "+family.containsAll(family));
+        family.remove(family1); 
+        System.out.println(family);
+
+        Object[] arr=family.toArray();
+
+        for(Object ob : arr){
+            System.out.println(ob);
+             
+        }
+        System.out.println("------------------------------");
+
+        String names[]=new String[family.size()];
+
+        names=family.toArray(names);
+        for(String name:names){
+            System.out.println(name);
+        }
+
+        Iterator<String> itr=family.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+            
+        }
+
         
 
 
