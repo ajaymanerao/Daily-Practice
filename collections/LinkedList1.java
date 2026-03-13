c2739017fbb8f1aa256af6dc50634640bb0fa09b
Ajay Madhavrao Anerao <162011264+ajaymanerao@users.noreply.github.com>
2026-03-13 14:39:45 +0530

    a

---
+++ collections/LinkedList1.java
@@ -0,0 +1,35 @@
+package collections;
+import java.util.*;
+
+public class LinkedList1 {
+    public static void main(String[] args) {
+    LinkedList<String>movie=new LinkedList<String>();
+    movie.offer("Sholey");
+    movie.offer("KGF");
+    movie.offer("RRR");
+    movie.offer("Toxic");
+    movie.offer("Pushpa");
+
+
+    System.out.println(movie);
+
+    movie.removeFirst();
+    System.out.println(movie);
+
+    movie.removeLast();
+    System.out.println(movie);
+
+    movie.offerFirst("Sairat");
+    System.out.println(movie);
+
+    movie.offerLast("Fandry");
+    System.out.println(movie);
+
+
+    
+
+
+
+    
+}
+}
